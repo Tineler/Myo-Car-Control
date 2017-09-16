428680bd7cc4015d54af2b63e2ebb1bc3839b99e
Martin Haas <martin.haas@adnexo.ch>
2017-09-16 02:32:39 +0200

    Initial commit

---
+++ Myo-Car-Control/Program.cs
@@ -0,0 +1,22 @@
+﻿using KopernikusWrapper;
+
+namespace MyoCarControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vehicle = new Vehicle("localhost");
+            while (vehicle.connectState == Vehicle.ConnectionState.AttemptingConnect)
+            {
+            }
+
+            while (true)
+            {
+                vehicle.SetThrottle(0.8f);
+                vehicle.SetSteeringAngle(0.5f);
+                vehicle.Update();
+            }
+        }
+    }
+}
